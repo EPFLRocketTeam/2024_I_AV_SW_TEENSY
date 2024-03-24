df2e971fccc430def976bb98e60ee22026b4c5ad
Owhenthesaints <owen.simon@epfl.ch>
2024-03-25 00:15:29 +0100

    build error just proto for overlay

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(void)
     const struct device *spi;
 
     // this part is modified from the original code
-    spi = device_get_binding(DEVICE_DT_NAME(DT_N_NODELABEL_flexspi));
+    spi = device_get_binding(DEVICE_DT_NAME(DT_N_NODELABEL_lpspi3));
     if (!device_is_ready(spi)) {
         return 0;
     }
