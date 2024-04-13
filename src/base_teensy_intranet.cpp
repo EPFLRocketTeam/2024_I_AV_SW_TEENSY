996dba7c8d860435278594100c5c8dc9d92f14ef
Owhenthesaints <owen.simon@epfl.ch>
2024-04-14 01:11:38 +0200

    prj.conf change + added all needed async vars to .hpp

--- src/base_teensy_intranet.cpp
+++ src/base_teensy_intranet.cpp
@@ -12,9 +12,12 @@
 
 BaseTeensyIntranet::BaseTeensyIntranet() : spi_cfg{FREQUENCY, SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB
                                                               | SPI_WORD_SET(8) | SPI_MODE_CPHA, SLAVE_LIMIT,
-                                                   CHIP_SELECT_OPT} {}
+                                                   CHIP_SELECT_OPT},
+                                           spi(device_get_binding(DEVICE_DT_NAME(DT_NODELABEL(lpspi4)))) {}
 
 
 bool BaseTeensyIntranet::readByte(uint8_t &byte) {
+    // doc
+    spi_read_signal(spi, spi_cfg,)
     return true;
 }
--- src/base_teensy_intranet.hpp
+++ src/base_teensy_intranet.hpp
@@ -12,14 +12,24 @@
 class BaseTeensyIntranet : public virtual BaseIntranetChannel {
 public:
     BaseTeensyIntranet();
+
     virtual void init() = 0;
-    bool readByte(uint8_t& byte) override;
+
+    bool readByte(uint8_t &byte) override;
+
 protected:
+    // this is the buffer
+    struct spi_buf *read_buffer;
+    // this points to an array of spi_buffs
+    const struct spi_buf *read_buffers;
+    const struct spi_buf_set *rx_bufs;
+    struct k_poll_signal *sig;
+private:
     const struct spi_config spi_cfg;
+    const struct device *spi;
 
 
 };
-
 
 
 #endif //APP_BASETEENSYINTRANET_H
